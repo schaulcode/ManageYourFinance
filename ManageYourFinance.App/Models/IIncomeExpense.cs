2b8432ee55904fa0cc89095e4fe4716c53b6e1d5
Schaul <schaulrotenberg@gmail.com>
2020-11-11 19:47:29 +0000

    Basic files and models for Income and Expense page added

---
+++ ManageYourFinance.App/Models/IIncomeExpense.cs
@@ -0,0 +1,18 @@
+﻿using ManageYourFinance.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageYourFinance.App.Models
+{
+    interface IIncomeExpense
+    {
+        string Name { get; set; }
+        int Total { get; set; }
+        int Average { get; set; }
+        double Precent { get; set; }
+        CategoryType IncomeOrExpense { get; set; }
+    }
+}
---
+++ ManageYourFinance.App/Models/IncomeExpensePayee.cs
@@ -0,0 +1,17 @@
+﻿using ManageYourFinance.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManageYourFinance.App.Models
+{
+    public class IncomeExpensePayee : IIncomeExpense
+    {
+        public string Name { get; set; }
+        public int Total { get; set; }
+        public int Average { get; set; }
+        public double Precent { get; set; }
+        public CategoryType IncomeOrExpense { get; set; }
+    }
+}
